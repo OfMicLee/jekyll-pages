9ab22d3288c538e270d588fdd71c752177c0d41e
liwenjun <liwenjun@qianmi.com>
2015-08-25 16:31:34 +0800

    Update post.js

--- js/post.js
+++ js/post.js
@@ -23,12 +23,15 @@ $(document).ready(function(){
 
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    window.disqus_shortname = 'micleewj'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'micleewj';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
